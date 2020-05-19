b98ec78b57af98bb6bae5389d431bf1d428a4647
Hosho Kusunose <hosho.kusu@icloud.com>
2020-05-19 22:00:35 +0900

    Initial Commit

---
+++ Original Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Original Application
+//
+//  Created by Kusunose Hosho on 2020/05/19.
+//  Copyright © 2020 Kusunose Hosho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
