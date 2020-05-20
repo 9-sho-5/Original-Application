82ddae6f3f5dd128a999b078912f876c6909fc01
Hosho Kusunose <hosho.kusu@icloud.com>
2020-05-21 00:28:58 +0900

    Day1 create edit sort back buttons

--- Original Application/ViewController.swift
+++ Original Application/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var table: UITableView!
+    
+    var models = [
+        "first",
+        "second",
+        "third"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row]
+        cell.textLabel?.font = UIFont(name: "Arial", size: 22)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        models.swapAt(sourceIndexPath.row, sourceIndexPath.row)
+        
+        let moveObjTmp = models[sourceIndexPath.row]
+        models.remove(at: sourceIndexPath.row)
+        models.insert(moveObjTmp, at: destinationIndexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            models.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    
+    
+    @IBAction func didTapSort() {
+        if table.isEditing {
+            table.isEditing = false
+        } else {
+            table.isEditing = true
+        }
+    }
 }
 
